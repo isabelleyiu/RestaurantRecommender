049ac7a82408a4ab846b44338457728ea5a588b9
Isabelle Yiu <isabelle.yiu.jobs@gmail.com>
2019-02-04 16:52:55 -0800

    Implement add restaurant, user, category, and rating functionality

--- restaurant_recommender.js
+++ restaurant_recommender.js
@@ -1,46 +1,108 @@
-class RestaurantRecommender() {
-  constructor() {
+// class Restaurant {
+//   constructor(name, category) {
+//     this.name = name;
+//     this.category = category;
+//   }
+// }
+
+// Restaurant.prototype.getAverageScore = function(scores) {
+
+// }
+
+// class User {
+//   constructor(name) {
+//     this.name = name;
+
+//   }
+// }
+
+// class Category {
+//   constructor(type) {
+//     this.type = [];
+//   }
+// }
+
+class RestaurantRecommender{
+  constructor (ratings, restaurants, users, categories) {
   // All main properties should go here.
-  this.ratings = {};
-  this.restaurants = {};
-  this.users = {};
+  this.ratings = [];
+  this.restaurants = [];
+  this.users = [];
+  this.categories = {};
   }
 
-  addRestaurant() {
+  addRestaurant(name, category) {
   // Adds a new Restaurant to the System
+  this.restaurants.push({
+    name: name,
+    category: category
+  });
   }
 
-  addUser() {
+  addUser(name) {
   // Adds a new User to the System
+    this.users.push({
+      name : name
+    });
   }
 
-  addCategory() {
+  addCategory(category) {
   // Adds a new Category to the System
+    this.categories[category] = [];
   }
 
-  addRating(){
+  addRating(restaurant, user, rating){
   // Adds a user rating for a restaurant
+    this.ratings.push({
+      restaurant : restaurant,
+      user : user, 
+      rating : rating
+    });
   }
 
-  addRestauranttoCategory(){
+  addRestauranttoCategory(restaurant, category){
   // Adds Restaurant to Category
   // Adds Category to Restaurant
+    if(this.categories[category]) {
+      this.categories[category].push(restaurant);
+    } else {
+      this.categories[category] = [];
+      this.categories[category].push(restaurant);
+    }
+    
+    this.restaurants.forEach(function(obj){
+      if(obj.name === restaurant) {
+        obj.category = category;
+      }
+    });
   }
 
 
-  deleteUser() {
-  // Deletes a User from the system
-  // Deletes User ratings for a restaurant.
-  }
+  // deleteUser() {
+  // // Deletes a User from the system
+  // // Deletes User ratings for a restaurant.
+  // }
 
-  deleteRestaurant() {
-  // make sure to also delete a restaurant from the categories.
-  }
+  // deleteRestaurant() {
+  // // make sure to also delete a restaurant from the categories.
+  // }
 
-  deleteCategory() {
-  // make sure to delete categories from a restaurant that had them.
-  }
+  // deleteCategory() {
+  // // make sure to delete categories from a restaurant that had them.
+  // }
 
-  filter(){
-  }
+  // filter(){
+  // }
 }
+
+
+
+
+var yelp = new RestaurantRecommender ('ratings', 'restaurants', 'users', 'categories')
+yelp.addRating('abcRestaurant', 'isabelle', 99);
+yelp.addRestaurant('McDonalds', 'fastfood');
+yelp.addUser('isabelle');
+yelp.addCategory('fastfood');
+yelp.addRestauranttoCategory('Panda Express', 'Chinese');
+yelp.addRestauranttoCategory('McDonalds', 'fastfood');
+console.log(yelp);
