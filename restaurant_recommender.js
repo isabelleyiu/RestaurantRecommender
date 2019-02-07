daccb6fd663c5a9a0744e52f6cb5744b43131c2c
Isabelle Yiu <isabelle.yiu.jobs@gmail.com>
2019-02-06 17:01:47 -0800

    Implement delete user function

--- restaurant_recommender.js
+++ restaurant_recommender.js
@@ -23,9 +23,7 @@ class Restaurant {
 }
 
 class User {
-  constructor(firstName, lastName, userName, id) {
-    this.firstName = firstName;
-    this.lastName = lastName;
+  constructor(userName, id) {
     this.userName = userName;
     this.id = id;
     this.ratings = {};
@@ -60,11 +58,24 @@ class RestaurantRecommender{
     this.users.push(user);
   }
 
-  // deleteUser(userid) {
-  // // Deletes a User from the system
+  deleteUser(userName) {
+    // Deletes a User from the system
+    var userId;
+    this.users.forEach(function(user){
+      if(user.userName === userName) {
+        userId = user.id
+      }
+    });
+    this.users.splice(userId, 1);
 
-  // // Deletes User ratings for a restaurant.
-  // }
+    // Deletes User ratings for a restaurant.
+    this.restaurants.forEach(function(restaurant){
+      if(restaurant.ratings[userName]) {
+        delete restaurant.ratings[userName];
+      }
+    });
+
+  }
 
   addRestaurant(restaurant) {
     // Adds a new Restaurant to the System
@@ -85,16 +96,17 @@ class RestaurantRecommender{
     var user = this.users.find(function(obj){
       return obj.userName === rating.user;
     });
-
+    // this should update rating 
     restaurant.ratings[rating.user] = rating.score;  
     user.ratings[rating.restaurant] = rating.score;
   }
 
-  updateRating(rating) {
-    this.sumRatings += rating;
-    this.numRatings++;
-    this.averageRating = sumRatings/numRatings;
-  }
+  // updateRating(rating) {
+  //   this.sumRatings += rating;
+  //   this.numRatings++;
+  //   this.averageRating = sumRatings/numRatings;
+  // }
+
   // deleteRestaurant() {
   // // make sure to also delete a restaurant from the categories.
   // }
@@ -109,27 +121,33 @@ class RestaurantRecommender{
 
 
 var yelp = new RestaurantRecommender ();
-var isabelle = new User('Isabelle', 'Yiu', 'isabelleyiu', yelp.users.length);
+var isabelle = new User('isabelleyiu', yelp.users.length);
 yelp.addUser(isabelle);
+var kelly = new User('kelly', yelp.users.length);
+yelp.addUser(kelly);
 var rintaro = new Restaurant('Rintaro',  yelp.restaurants.length, 'Japanese');
 yelp.addRestaurant(rintaro);
 var panda = new Restaurant('Panda Express',  yelp.restaurants.length, 'Chinese');
 yelp.addRestaurant(panda);
 var rintaroRating = new Rating('Rintaro', 'Japanese', 'isabelleyiu', 95);
 yelp.addRating(rintaroRating);
+var pandaRating = new Rating('Panda Express', 'Chinese', 'kelly', 95);
+yelp.addRating(pandaRating);
 console.log(rintaro.numRatings());
-
+yelp.deleteUser('isabelleyiu');
 console.log(yelp);
 
 // $(document).ready(function(){
 
 //   var yelp = new RestaurantRecommender ();
-//   console.log(yelp);
-//   $('#signup').submit(function(event){
-    
-//     var username = $('#username').val();
-//     console.log(username);
-//     var newUser = new User('Isabelle', 'Yiu', username, yelp.length);
+  
+//   $('#userForm').on('submit', function(event){
+//     event.preventDefault();
+//     var $username = $('#username').val();
+//     var newUser = new User($username, yelp.users.length);
 //     yelp.addUser(newUser);
 //   });
+
+
+//   console.log(yelp);
 // });
