1f9ae8db0f1f6ae1cbead3114dbe40cd986d08e1
Isabelle Yiu <isabelle.yiu.jobs@gmail.com>
2019-02-07 16:01:32 -0800

    Add test cases for User class

--- spec/restaurant_recommender_spec.js
+++ spec/restaurant_recommender_spec.js
@@ -8,6 +8,7 @@ describe("RestaurantRecommender", function() {
 
   describe("User", function() {
     it("should add a user object to User", function() {
+      expect(rr.users.length).toEqual(0);
       rr.addUser('Isabelle');
       expect(rr.users.length).toEqual(1);
     });
@@ -16,5 +17,25 @@ describe("RestaurantRecommender", function() {
       rr.addUser('Isabelle');
       expect(rr.users[0].userName).toEqual('Isabelle');
     });
+
+    it("should delete user object with a method deleteUser(userName)", function() {
+      rr.addUser('isabelleyiu');
+      rr.addUser('kelly');
+      expect(rr.users.length).toEqual(2);
+      rr.deleteUser('kelly');
+      expect(rr.users.length).toEqual(1);
+    });
   });
+
+  // describe("Restaurant", function() {
+  //   it("should add a user object to User", function() {
+  //     rr.addUser('Isabelle');
+  //     expect(rr.users.length).toEqual(1);
+  //   });
+
+  //   it("should set username", function() {
+  //     rr.addUser('Isabelle');
+  //     expect(rr.users[0].userName).toEqual('Isabelle');
+  //   });
+  // });
 });
