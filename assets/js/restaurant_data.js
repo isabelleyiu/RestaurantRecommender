119fd9e41084c76487319847a8a3923d29369b39
Isabelle Yiu <isabelle.yiu.jobs@gmail.com>
2019-02-08 10:18:27 -0800

    Add dummy data for restaurants

---
+++ assets/js/restaurant_data.js
@@ -0,0 +1,34 @@
+var dummyData = [
+  {
+    name: "Taco Bell",
+    category: "Fast Food"
+  },
+  {
+    name: "Super Duper Burger",
+    category: "Fast Food"
+  },
+  {
+    name: "Jalsa Catering",
+    category: "Indian"
+  },
+  {
+    name: "Little Vietnam",
+    category: "Vietnamese"
+  },
+  {
+    name: "Hongry Kong",
+    category: "Chinese"
+  },
+  {
+    name: "Phat Philly",
+    category: "Sandwich"
+  },
+  {
+    name: "Slider Shack",
+    category: "Hawaiian"
+  },
+  {
+    name: "Cheesecake Factory",
+    category: "American"
+  }
+]
