cd0a669880d7fb8f08a0ff8d7353f5da6a8a13f1
Isabelle Yiu <isabelle.yiu.jobs@gmail.com>
2019-02-09 23:46:49 -0800

    Change default score to 100

--- assets/js/restaurant_data.js
+++ assets/js/restaurant_data.js
@@ -3,48 +3,48 @@ var dummyData = [
     restaurant: "Taco Bell",
     category: "Fast Food",
     user: 'rr',
-    rating: 0
+    rating: 100
   },
   {
     restaurant: "Super Duper Burger",
     category: "Fast Food",
     user: 'rr',
-    rating: 0
+    rating: 100
   },
   {
     restaurant: "Jalsa Catering",
     category: "Indian",
     user: 'rr',
-    rating: 0
+    rating: 100
   },
   {
     restaurant: "Little Vietnam",
     category: "Vietnamese",
     user: 'rr',
-    rating: 0
+    rating: 100
   },
   {
     restaurant: "Hongry Kong",
     category: "Chinese",
     user: 'rr',
-    rating: 0
+    rating: 100
   },
   {
     restaurant: "Phat Philly",
     category: "Sandwich",
     user: 'rr',
-    rating: 0
+    rating: 100
   },
   {
     restaurant: "Slider Shack",
     category: "Hawaiian",
     user: 'rr',
-    rating: 0
+    rating: 100
   },
   {
     restaurant: "Cheesecake Factory",
     category: "American",
     user: 'rr',
-    rating: 0
+    rating: 100
   }
 ]
